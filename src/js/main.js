c31ca7b40795ddcddd7623ba175f4e8607e6e475
emilymeng <eng.emilym@gmail.com>
2019-02-12 20:54:53 -0800

    images in

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,31 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+require("react-flip-toolkit");
+require("React");
+
+// import React, { Component } from 'React';
+// import { Flipper, Flipped } from 'react-flip-toolkit';
+
+class AnimatedSquare extends Component {
+  state = { fullScreen: false };
+
+  toggleFullScreen = () => {
+    this.setState(prevState => ({
+      fullScreen: !prevState.fullScreen
+    }));
+  };
+
+  render() {
+    return (
+      <Flipper flipKey={this.state.fullScreen}>
+        <Flipped flipId="square">
+          <div
+            className={this.state.fullScreen ? "full-screen-square" : "square"}
+            onClick={this.toggleFullScreen}
+          />
+        </Flipped>
+      </Flipper>
+    );
+  }
+}
